458648c3a1dd3aa8032e4e802caf50bb0734befd
ronbruce <ronjamalbruce@gmail.com>
2022-09-14 17:15:02 -0400

    Textarea hides when I click hide notes. Textarea shows when I click show notes

--- movie.js
+++ movie.js
@@ -14,28 +14,34 @@ template.innerHTML = `
 class CommentBox extends HTMLElement {
     constructor() {
         super();
-
       this.showtextarea  = true;
       this.attachShadow({ mode: "open" });
       this.shadowRoot.appendChild(template.content.cloneNode(true));
+      
    
 }
 
 toggleInfo() {
  this.showtextarea = !this.showtextarea;
 
+ const info = this.shadowRoot.querySelector("#mytextarea");
  const toggleBtn = this.shadowRoot.querySelector("#toggle-info");
 
  if(this.showtextarea) {
-    
+    info.style.display = "block";
     toggleBtn.innerText = "Hide Notes";
  } else {
-   
+    info.style.display = "none";
     toggleBtn.innerText = "Show Notes";
     
  }
 
 }
+
+// 1. Life cycle method
+// 2. In my life cycle method I'm calling this.toggleInfo
+// 3. Create toggle info within class
+
 connectedCallback() {
     this.shadowRoot.querySelector('#toggle-info').addEventListener('click', () => this.toggleInfo());
   }

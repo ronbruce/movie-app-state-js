fc71804c0c26beed814c4876ee63c81f862bc64b
Ron Bruce <101830036+ronbruce@users.noreply.github.com>
2022-09-14 17:33:20 -0400

    Delete database.js
    This file was deleted because it was only for reference purposes.

--- database.js
+++
@@ -1,9 +0,0 @@
-import { openDB } from 'idb';
-
-export const database = openDB('movie-app', 1, {
-  upgrade(db) {
-    db.createObjectStore('movies', { keyPath: 'imdbID' });
-    db.createObjectStore('settings');
-    db.createObjectStore('notes', { keyPath: 'imdbID' });
-  },
-});

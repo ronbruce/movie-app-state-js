ec4ae56e6174a406a3723945574f7e08e0566b08
ronbruce <ronjamalbruce@gmail.com>
2022-09-14 16:12:42 -0400

    I added a function that allows me to show and hide notes. However, I want to show and hide text area. So this is my goal on the next branch.

--- movie.js
+++ movie.js
@@ -3,9 +3,10 @@ import './main.js';
 const template = document.createElement("template");
 template.innerHTML = `
 <div class="comment-box>
-<div class="comment-edit>
-<textarea class="comment-input" rows="4" cols="30"></textarea>
-<button class="toggle-notes">Notes</button>
+<div class="comment-info>
+<textarea rows="4" cols="30" id="mytextarea"></textarea>
+<button id="toggle-info">Hide Notes</button>
+
 </div>
 </div>
 `
@@ -13,13 +14,36 @@ template.innerHTML = `
 class CommentBox extends HTMLElement {
     constructor() {
         super();
+
+      this.showtextarea  = true;
       this.attachShadow({ mode: "open" });
       this.shadowRoot.appendChild(template.content.cloneNode(true));
-      
-  
+   
 }
 
+toggleInfo() {
+ this.showtextarea = !this.showtextarea;
+
+ const toggleBtn = this.shadowRoot.querySelector("#toggle-info");
+
+ if(this.showtextarea) {
+    
+    toggleBtn.innerText = "Hide Notes";
+ } else {
+   
+    toggleBtn.innerText = "Show Notes";
+    
+ }
 
 }
+connectedCallback() {
+    this.shadowRoot.querySelector('#toggle-info').addEventListener('click', () => this.toggleInfo());
+  }
+
+  disconnectedCallback() {
+    this.shadowRoot.querySelector('#toggle-info').removeEventListener();
+  }
+}
+
 
 window.customElements.define("comment-box", CommentBox);
